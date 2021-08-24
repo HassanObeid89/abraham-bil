dd11583a51676dc862ca7ccb128215acae98025e
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-08-24 13:53:55 +0200

    added the instruction section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import HeroSection from "./components/HeroSection";
-
+import Instruction from './components/Instruction'
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
-      
+      <Instruction />
     </div>
   );
 }
---
+++ src/components/Instruction.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../App.css";
+
+export default function Instruction() {
+    return (
+      <div className="instruction-Container">
+        <article>
+          <p>Fyll i formuläret nedan så kontaktar vi dig!</p>
+          <p>
+            Det går också bra att ringa Abraham direkt på{" "}
+            <span>070-465 42 29</span>. Värderingen är helt gratis och du binder
+            dig ej till något!
+          </p>
+        </article>
+      </div>
+    );
+}
+
