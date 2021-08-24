f54d8358b31536ed9b421181c57b00c9781d9383
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-08-24 15:38:01 +0200

    Added the Contact form use Formik library and validation using Yup.

---
+++ src/components/TextField.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+import { ErrorMessage, useField } from 'formik'
+
+export const TextField = ({ label, ...props }) => {
+
+    const [field, meta] = useField(props)
+
+    return (
+      <div>
+        <label htmlFor={field.name}>{label}</label>
+        <input {...field} {...props} autoComplete="off" />
+        <ErrorMessage component='div' className='error' name={field.name} />
+      </div>
+    );
+}
